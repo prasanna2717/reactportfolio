5876bb3d2f9b175cd1871ea894e3b68d2bcc4bf5
prasanna2717 <prasannakumarr022002@gmail.com>
2025-07-01 23:42:44 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './componenets/Navebar';
 import About from './componenets/About';
